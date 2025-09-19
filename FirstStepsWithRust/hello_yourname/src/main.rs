08424a50b87f05bb10f144d871107af99a3e79e7
IOT8 <qcjxberin@outlook.com>
2025-09-19 19:41:19 +0800

    重构代码，将获取用户姓名的逻辑提取到单独的函数中

--- FirstStepsWithRust/hello_yourname/src/main.rs
+++ FirstStepsWithRust/hello_yourname/src/main.rs
@@ -1,13 +1,17 @@
 use std::io::stdin;
 
+fn what_is_your_name() -> String {
+ let mut your_name = String::new();
+ stdin()
+        .read_line(&mut your_name)
+        .expect("Failed to read line");
+    your_name
+}
+
 fn main() {
     println!("Hello, what's your name?");
 
-    let mut your_name = String::new();
+   let name = what_is_your_name();
 
-    stdin()
-        .read_line(&mut your_name)
-        .expect("Failed to read line");
-
-    println!("Hello, {}", your_name);
+    println!("Hello, {}", name);
 }
